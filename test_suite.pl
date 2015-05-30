c2ea7f364ada5e3595374777c3ee23473537329c
rporrini <r.porrini@gmail.com>
2015-05-30 17:43:36 +0200

    now we parse arbitrary files

--- test_suite.pl
+++ test_suite.pl
@@ -1,6 +1,18 @@
+:- use_module(library(semweb/rdf_db)).
+:- use_module(summary).
+
 :- begin_tests(suite).
 
-test(always_passing) :- true .
+create_rdf_file(File) :- 
+	tmp_file('test', File),
+	open(File, write, Stream),
+	write(Stream, '<subject> <predicate> <object> .'),
+	nl(Stream),
+	close(Stream).
+test('rdf files should be loaded', [setup(create_rdf_file(File)), cleanup(delete_file(File))]) :- 
+	load(File),
+	rdf(subject, predicate, object).
+test('should always pass') :- true.
 
 :- end_tests(suite).
 
