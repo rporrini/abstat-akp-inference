fb381b7a402604e22aa5aaf8157ff9bdc18dec79
rporrini <r.porrini@gmail.com>
2015-05-30 18:23:22 +0200

    refactoring

--- test_suite.pl
+++ test_suite.pl
@@ -3,15 +3,20 @@
 
 :- begin_tests(suite).
 
-create_rdf_file(File) :- 
-	tmp_file('test', File),
+file(File, Lines) :- 
+	tmp_file(test, File),
 	open(File, write, Stream),
-	write(Stream, '<subject> <predicate> <object> .'),
-	nl(Stream),
+	line(Stream, Lines),
 	close(Stream).
-test('rdf files should be loaded', [setup(create_rdf_file(File)), cleanup(delete_file(File))]) :- 
+line(Stream, [Line|_]) :-
+	write(Stream, Line),
+	nl(Stream).
+
+test('an rdf file with a single line should be loaded', [cleanup(delete_file(File))]) :- 
+	file(File, ['<subject> <predicate> <object> .']),
 	load(File),
 	rdf(subject, predicate, object).
+
 test('should always pass') :- true.
 
 :- end_tests(suite).
