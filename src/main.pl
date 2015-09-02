f3e3237442451f91c1bd710bef4d7dc5a49e6e24
rporrini <r.porrini@gmail.com>
2015-09-02 18:40:59 +0200

    aaa

--- src/main.pl
+++ src/main.pl
@@ -1,10 +1,17 @@
 :- use_module(summary).
 
-:- load('summaries/dbpedia-2014/concept-graph.nt').
-:- load('summaries/dbpedia-2014/akp-datatype.nt').
-:- load('summaries/dbpedia-2014/akp-object-00.nt').
-:- load('summaries/dbpedia-2014/akp-object-01.nt').
-:- load('summaries/dbpedia-2014/akp-object-02.nt').
-:- load('summaries/dbpedia-2014/akp-object-03.nt').
-:- load('summaries/dbpedia-2014/akp-object-04.nt').
+main :-
+	writeln("loading files"),
+	load("summaries/dbpedia-2014/concept-graph.nt"),
+	load("summaries/dbpedia-2014/akp-datatype.nt"),
+	load("summaries/dbpedia-2014/akp-object-00.nt"),
+	load("summaries/dbpedia-2014/akp-object-01.nt"),
+	load("summaries/dbpedia-2014/akp-object-02.nt"),
+	load("summaries/dbpedia-2014/akp-object-03.nt"),
+	load("summaries/dbpedia-2014/akp-object-04.nt"),
+	writeln("done"),
+	writeln("starting writing"),
+	open("file.txt", write, Stream, [buffer(line)]),
+	forall(akp(X, _ ,_), write(Stream, X)),
+	close(Stream).
 
