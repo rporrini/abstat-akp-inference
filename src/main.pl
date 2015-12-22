810848506bb6961f25e089aacddc99b63759a3a5
rporrini <r.porrini@gmail.com>
2015-12-22 18:18:36 +0100

    added the new test data

--- src/main.pl
+++ src/main.pl
@@ -1,13 +1,9 @@
 :- use_module(summary).
 
 load_all :-
-	writeln("Loading the summary"),
-	load("summaries/dbpedia-2014/concept-graph.nt"),
-	load("summaries/dbpedia-2014/akp-datatype.nt"),
-	load("summaries/dbpedia-2014/akp-object-00.nt"),
-	load("summaries/dbpedia-2014/akp-object-01.nt"),
-	load("summaries/dbpedia-2014/akp-object-02.nt"),
-	load("summaries/dbpedia-2014/akp-object-03.nt"),
-	load("summaries/dbpedia-2014/akp-object-04.nt"),
+	writeln("Loading the triples"),
+	load("summaries/dbpedia-2014/concept-graph-cleaned.nt"),
+	load("summaries/dbpedia-2014/directors.nt"),
+	load("summaries/dbpedia-2014/minimal-types.nt"),
 	writeln("Done").
 
